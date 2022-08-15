cbcff4eb8d3fcbc97cbf18a535e2bdc5a955fcc5
MagaVigna <110397043+MagaVigna@users.noreply.github.com>
2022-08-15 13:44:08 +0530

    Add files via upload

---
+++ poll.py
@@ -0,0 +1,18 @@
+i=10
+number=10
+count=0 #to count the number of students
+while(i>=10 ): #loop to continue the next statements till i becomes 10 or number becomes 4
+    print("OPINION POLL")
+    print("Enter 1. for online class")
+    print("2. in person class")
+    print("3. mixed")
+    print("4. No comment")
+    number=int(input())
+    if(number==4):
+        break
+    print("Enter Male or Female:") #to check the gender for further use
+    gender=input()
+    if(gender=="Female" and number==1):
+        count=count+1
+    i=i+1
+print("The percentage of females who prefer online class is:")
