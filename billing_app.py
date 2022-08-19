8ac45c340d0f92534b3f6fa16b2bc57a7fe3ecad
Maga <magavigna1053@gmail.com>
2022-08-19 20:12:22 +0530

    Adding Comments

--- billing_app.py
+++ billing_app.py
@@ -1,14 +1,14 @@
-coffee=100
+coffee=100 #coffee rate
 coffee_count=0
-vadai=100
+vadai=100 #vadai rate
 vadai_count=0
-sandwich=200
+sandwich=200 #sandwich rate
 sandwich_count=0
 coke=60
-coke_count=0
+coke_count=0 #coke rate
 bill=0
 menu_choice="yes"
-while(menu_choice=="yes"):
+while(menu_choice=="yes"): #displaying the menu
     print("Menu")
     print("1. Coffee - Rs 100")
     print("2. Vadai(1) - Rs 100")
@@ -28,7 +28,7 @@ while(menu_choice=="yes"):
         bill+=coke
         coke_count+=1
     menu_choice=input("Do you want to continue(Type yes):")
-if(sandwich_count>1 or vadai_count>2):
+if(sandwich_count>1 or vadai_count>2): #calculating discounts
     coffee_discount=50
     if(coffee_count>0):
         while(coffee_count>0):
