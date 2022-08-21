0f2e7c7eb349b0ef8ce905b4d4b21f89b70d1aab
Maga <magavigna1053@gmail.com>
2022-08-21 23:19:38 +0530

    Bug Fixes

--- Lists/class_average.py
+++ Lists/class_average.py
@@ -1,16 +1,16 @@
 
-no_of_students=int(input("Enter the total number of students:"))
+no_of_students=int(input("Enter the total number of students:")) #getting number of students as input
 name_of_student=[]
 marks_of_student=[]
 for student in range (0,no_of_students):
-    name=input("Enter your name:")
+    name=input("Enter your name:") #getting names from first to last
     name_of_student.append(name)
-name_of_student.reverse()
+name_of_student.reverse() #reversing the list
 for student in range(0,no_of_students):
     print("Enter your mark",name_of_student[student],": ")
-    mark=int(input())
+    mark=int(input()) #getiing the marks in reverse order
     marks_of_student.append(mark)
 total=sum(marks_of_student)
-class_average=total/no_of_students
+class_average=total/no_of_students #calculating average
 print("Class average is:",class_average)
     
