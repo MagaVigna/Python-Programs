4b2e10682690b592cc6af58d444e75d9c8fff810
Maga <magavigna1053@gmail.com>
2022-08-22 01:24:23 +0530

    HW

---
+++ Strings/chars_in_both_strings.py
@@ -0,0 +1,7 @@
+string1=input("Enter an string:") #getting two strings as input from the user
+string2=input("Enter an string:")
+for str1 in range(0,len(string1)): #comparing the charecters of the first string with every charecter of second string
+    for str2 in range(0,len(string2)):
+        if string1[str1]==string2[str2]:
+            print(string1[str1])
+            break
