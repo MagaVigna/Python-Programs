7eaed1117ce832decd8936fc9e7537fc37ce2381
Maga <magavigna1053@gmail.com>
2022-08-27 16:02:39 +0530

    Bug Fixes

--- Strings/printing_letters_btw_FirstA_lastA.py
+++ Strings/printing_letters_btw_FirstA_lastA.py
@@ -2,28 +2,35 @@ word1=input("Enter a string:")
 word=word1.upper()
 A1_flag=0 #to check the presence of first A
 A2_flag=0 #to check the presence of last A
-for first in range (0,len(word)): #to find the first index
+for first in range (0,len(word)-1): #to find the first index
     if word[first]=='A':
         first_index=first
         A1_flag=1
+        print(first_index)
+        word=word.replace('A',' ')
+        print(word[first_index])
         break
 
 for last in reversed(range (0,len(word))): #to find the last index
     if word[last]=='A':
         last_index=last
         A2_flag=1
+        print(last_index)
+            #word.replace('A','')
         break
 
 if A1_flag and A2_flag: #printing letters between first and last index
     for number in range(first_index+1,last_index):
         print(word[number])
+elif A1_flag:
+    print("There is no last A in the word")
+elif A2_flag:
+    print("There is no first A in the word")
 else:
     print("There is no A in the word")
 
 '''Output:
-Enter a string:nbba
-N comes before A'''
-'''Enter a string:abbbaaa
+Enter a string:abbbaaa
 B
 B
 B
