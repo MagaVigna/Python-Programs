31c7a876e6e9dd7acae1fdec48b3988c5a3c4e42
Maga <magavigna1053@gmail.com>
2022-08-27 16:25:23 +0530

    Bug Fixes

--- Strings/printing_letters_btw_A_and_N.py
+++ Strings/printing_letters_btw_A_and_N.py
@@ -2,22 +2,25 @@ word1=input("Enter a string:")
 word=word1.upper()
 A_flag=0 #to check if A is present in the word
 N_flag=0 #to check if N is present in the word
-for first in range (0,len(word)): #finding first index
+N2_flag=0
+for first in range (0,len(word)-1): #finding first index
     if word[first]=='A':
         first_index=first
         A_flag=1
         break
-    elif word[first]=='N':
-        print("N comes before A")
-        exit()
+    if word[first]=='N':
+        N_flag=1
+        N2_flag=1
+        break
 
 for last in reversed(range (0,len(word))): #finding last index
     if word[last]=='N':
         last_index=last
         N_flag=1
         break
-
-if A_flag and N_flag: #printing letter between first and last index
+if N2_flag:
+    print("N comes before A")
+elif A_flag and N_flag: #printing letter between first and last index
     for number in range(first_index+1,last_index):
         print(word[number])
 elif A_flag:
@@ -28,13 +31,15 @@ else:
     print("There is no A and N")
 
 ''' Output:
-Enter a string:abbbn
-B
+Enter a string:abbn
 B
 B'''
 
-'''Enter a string:abba
+'''Enter a string:abb
 There is no N in the word'''
+
+'''Enter a string:bbn
+There is no A in the word'''
 
 '''Enter a string:nbba
 N comes before A'''
