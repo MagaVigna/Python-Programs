541b6ab0de5dc916ce318ab60a29f4a9b7d5a3b4
Maga <magavigna1053@gmail.com>
2022-08-27 13:22:14 +0530

    Bug Fixes

--- Strings/printing_letters_btw_A_and_N.py
+++ Strings/printing_letters_btw_A_and_N.py
@@ -1,8 +1,8 @@
 word1=input("Enter a string:")
 word=word1.upper()
-A_flag=0
-N_flag=0
-for first in range (0,len(word)):
+A_flag=0 #to check if A is present
+N_flag=0 #to check if N is present
+for first in range (0,len(word)): #finding first index
     if word[first]=='A':
         first_index=first
         A_flag=1
@@ -11,13 +11,13 @@ for first in range (0,len(word)):
         print("N comes before A")
         exit()
 
-for last in reversed(range (0,len(word))):
+for last in reversed(range (0,len(word))): #finding last index
     if word[last]=='N':
         last_index=last
         N_flag=1
         break
 
-if A_flag and N_flag:
+if A_flag and N_flag: #printing letter between first and last index
     for number in range(first_index+1,last_index):
         print(word[number])
 elif A_flag:
