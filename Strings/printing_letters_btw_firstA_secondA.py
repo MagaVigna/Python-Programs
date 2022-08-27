407699bd46ad27d4fd53e0c07ad6615e9da425aa
Maga <magavigna1053@gmail.com>
2022-08-27 16:27:12 +0530

    Bug Fixes

--- Strings/printing_letters_btw_firstA_secondA.py
+++ Strings/printing_letters_btw_firstA_secondA.py
@@ -1,21 +1,22 @@
 word1=input("Enter a string:")
 word=word1.upper()
+first_index=0
 A1_flag=0 #to check the presence of first A
 A2_flag=0 #to check the presence of second A
-for first in range (0,len(word)): #to find the first index
+for first in range (0,len(word)-1): #to find the first index
     if word[first]=='A':
         first_index=first
         A1_flag=1
         break
 
-for second in range (first_index+1,len(word)): #to find the second index
-    if word[second]=='A':
-        last_index=second
+for second in range (0,len(word)): #to find the second index
+    if word[second]=='A' and first_index!=second:
+        second_index=second
         A2_flag=1
         break
 
 if A1_flag and A2_flag: #printing letters between first and second index
-    for number in range(first_index+1,last_index):
+    for number in range(first_index+1,second_index):
         print(word[number])
 elif A1_flag:
     print("There is no second A in the word")
@@ -32,3 +33,5 @@ B'''
 B'''
 '''Enter a string:abb
 There is no second A in the word'''
+'''Enter a string:bba
+There is no first A in the word'''
