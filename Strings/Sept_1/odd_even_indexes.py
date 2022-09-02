73a879b0186e36666c5574168570e77a2efe2a9c
Maga <magavigna1053@gmail.com>
2022-09-02 21:56:15 +0530

    BUG FIXES

--- Strings/Sept_1/odd_even_indexes.py
+++ Strings/Sept_1/odd_even_indexes.py
@@ -4,25 +4,30 @@
     Print all the chars in the even index in reverse order - eg input abcd output ca'''
 
 input_string=input("Enter any string:") #getting the input string
-print("The charecters in the even indexes:")
+print("The charecters in the odd indexes:")
 for odd_index in range(1,len(input_string),2): #loop to print the values in odd indexes
     print(input_string[odd_index])
-print("The charecters in the odd indexes:")
-for even_index in range(0,len(input_string),2): #loop to print the values in even indexes
+print("The charecters in the even indexes:")
+starting_index=-1
+if (len(input_string)%2==0): #checking if the length of the string is even initialising variable to start the loop from second last index
+    starting_index=len(input_string)-2
+else: #else initialising variable to start from last index
+    starting_index=len(input_string)-1
+for even_index in range(starting_index,-1,-2):
     print(input_string[even_index])
 
 '''Outputs:
-Enter any string:abcd
+Enter any string:1234
+The charecters in the odd indexes:
+2
+4
 The charecters in the even indexes:
+3
+1'''
+'''The charecters in the odd indexes:
 b
 d
-The charecters in the odd indexes:
-a
-c'''
-'''The charecters in the even indexes:
+The charecters in the even indexes:
 e
-l
-The charecters in the odd indexes:
-h
-l
-o'''
+c
+a'''
